ab09676a2d9652485d7190c2d4fbd34a97afe338
FJeffersonferia <126828472+FJeffersonferia@users.noreply.github.com>
2024-07-01 17:16:19 -0500

    Delete Js directory

--- Js/user.js
+++
@@ -1,9 +0,0 @@
-alert("Bienvenido Señor(a) usuario");
-const verificarRol = () => {
-    const rolAlmacenado = localStorage.getItem('rol');
-    if (rolAlmacenado!== 'Usuario') {
-        window.location.href = '../index.html';
-    }
-};
-
-verificarRol();
